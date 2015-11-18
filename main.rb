7824dc2d8cb2494d3b8419af0009a87d1b00fc5a
Anton Davydov <antondavydov.o@gmail.com>
2015-11-19 01:04:12 +0300

    Add detailed status for each user

--- main.rb
+++ main.rb
@@ -37,9 +37,7 @@ end
 
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
-    require 'debug'
     if Swearing.all_words.any?{ |w| message.text =~ /#{w}/ }
-      require 'debug'
       full_name = "#{message.from.first_name} #{message.from.last_name}"
       user = User.find_or_create name: full_name
       Offence.create word: $~, message: message.text, user_id: user.id
@@ -48,8 +46,13 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
 
     case message.text
     when '/status'
-      users_statu = User.all.map{|u| "#{u.name} - #{u.offences.count} offences" }.join("\n")
-      bot.api.send_message(chat_id: message.chat.id, text: "Current status:\n#{users_statu}")
+      users_status = User.all.map{|u| "#{u.name} - #{u.offences.count} offences" }.join("\n")
+      bot.api.send_message(chat_id: message.chat.id, text: "Current status:\n#{users_status}")
+    when /\/status/
+      user = User[name: message.text.sub(/\/status /, '')]
+      user_status = user.offences.group_by(&:word)
+        .map{ |(word, offences)| "  #{word} - #{offences.count}" }.join("\n")
+      bot.api.send_message(chat_id: message.chat.id, text: "Status for #{user.name}:\n#{user_status}")
     when /\/add/
       swearing = Swearing.find_or_create word: message.text.split.last
       bot.api.send_message(chat_id: message.chat.id, text: "Create new swearing - '#{swearing.word}'")
